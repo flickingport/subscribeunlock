9ba23fd0035f283b91c6167b228d18c358e80bc0
chacha <58000512+flickingport@users.noreply.github.com>
2022-12-23 19:25:38 +0800

    Update and rename sample1 to sample1.js

--- sample1.js
+++ sample1.js
@@ -1,3 +1,6 @@
-hostname =  gateway.caixin.com
 # ✅ 财新周刊
+[rewrite_local]
 http://ipadcms.caixin.com/tmp/articles/* url script-response-body https://github.com/ddgksf/Scripts/raw/dev/CaiXinZhouKanProCrack.js
+
+[mitm]
+hostname =  gateway.caixin.com
